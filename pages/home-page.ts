827f74b8391be65a9abcb897eeddf841a15bef5f
pedropimenta <ppimenta@gmail.com>
2025-04-10 10:33:39 +0100

    chapter3

--- pages/home-page.ts
+++ pages/home-page.ts
@@ -1,23 +1,28 @@
-import { type Locator, type Page, expect } from '@playwright/test';
+import { type Locator, type Page, expect } from "@playwright/test";
 
 export class HomePage {
-    readonly page: Page;
-    readonly getStartedButton: Locator;
-    readonly pageTitle: RegExp;
+  //variables
+  readonly page: Page;
+  // await page.getByRole("link", { name: "Get started" }).click();
+  readonly getStartedButton: Locator;
+  // await expect(page).toHaveTitle(/Playwright/);
+  readonly title: RegExp;
 
-    constructor(page: Page) {
-        this.page = page;
-        this.getStartedButton = page.getByRole('link', { name: 'Get started' });
-        this.pageTitle = /Playwright/;
-    }
+  //constructor
+  constructor(page: Page) {
+    this.page = page;
+    this.getStartedButton = page.getByRole("link", { name: "Get started" });
+    this.title = /Playwright/;
+  }
 
-    async clickGetStarted() {
-        await this.getStartedButton.click();
-    }
+  //methods
+  async clickGetStarted() {
+    await this.getStartedButton.click();
+  }
 
-    async assertPageTitle() {
-        await expect(this.page).toHaveTitle(this.pageTitle);
-    }
+  async assertPageTitle() {
+    await expect(this.page).toHaveTitle(this.title);
+  }
 }
 
 export default HomePage;
--- tests/example2.spec.ts
+++ tests/example2.spec.ts
@@ -1,46 +1,46 @@
-import { test, type Page } from '@playwright/test';
-import { HomePage } from '../pages/home-page';
-import { TopMenuPage } from '../pages/top-menu-page';
+import { test, expect, type Page } from "@playwright/test";
+import { HomePage } from "../pages/home-page";
 
-const URL = 'https://playwright.dev/';
+const URL = "https://playwright.dev/";
 let homePage: HomePage;
-let topMenuPage: TopMenuPage;
-const pageUrl = /.*intro/;
 
-test.beforeEach(async ({page}) => {
-    await page.goto(URL);
-    homePage = new HomePage(page);
+test.beforeEach(async ({ page }) => {
+  await page.goto(URL);
+  homePage = new HomePage(page);
 });
 
 async function clickGetStarted(page: Page) {
-    await homePage.clickGetStarted();
-    topMenuPage = new TopMenuPage(page);
+  //   await page.getByRole("link", { name: "Get started" }).click();
+  await homePage.clickGetStarted();
 }
 
-test.describe('Playwright website', () => {
+test.describe("Playwright tutorial", () => {
+  test("has title", async () => {
+    // Expect a title "to contain" a substring.
+    await homePage.assertPageTitle();
+  });
 
-    test('has title', async () => {
-        await homePage.assertPageTitle();
-    });
-    
-    test('get started link', async ({ page }) => {
-        // Act
-        await clickGetStarted(page);
-        // Assert
-        await topMenuPage.assertPageUrl(pageUrl);
-    });
-    
-    test('check Java page', async ({ page }) => {
-        await test.step('Act', async () => {
-            await clickGetStarted(page);
-            await topMenuPage.hoverNode();
-            await topMenuPage.clickJava();
-        });
-      
-        await test.step('Assert', async () => {
-            await topMenuPage.assertPageUrl(pageUrl);
-            await topMenuPage.assertNodeDescriptionNotVisible();
-            await topMenuPage.assertJavaDescriptionVisible();
-        });
-    });
+  test("get started link", async ({ page }) => {
+    // Click the get started link.
+    await clickGetStarted(page);
+
+    // Expects page to have a heading with the name of Installation.
+    await expect(
+      page.getByRole("heading", { name: "Installation" })
+    ).toBeVisible();
+  });
+
+  test.only("check java page pedro", async ({ page }) => {
+    await clickGetStarted(page);
+    await page.getByRole("button", { name: "Node.js" }).hover();
+    await page.getByText("Java", { exact: true }).click();
+
+    await expect(page).toHaveURL("https://playwright.dev/java/docs/intro");
+    await expect(
+      page.getByText("Installing Playwright", { exact: true })
+    ).not.toBeVisible();
+
+    const javaCopy = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
+    await expect(page.getByText(javaCopy)).toBeVisible();
+  });
 });
